43e7723c65ed68ed4e7f8f60f9ba99dd382c15da
linchunliang <stonelam@leoers.com>
2014-11-06 17:22:23 +0800

    temp

--- src/com/leo/appmaster/SDKWrapper.java
+++ src/com/leo/appmaster/SDKWrapper.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import android.content.Context;
 import android.util.Log;
 
+import com.baidu.mobstat.SendStrategyEnum;
+import com.baidu.mobstat.StatService;
 import com.flurry.android.FlurryAgent;
 import com.leo.appmaster.push.PushUIHelper;
 import com.leo.appmaster.update.UIHelper;
@@ -23,25 +25,30 @@ public class SDKWrapper {
     public static void iniSDK(Context ctx) {
         iniLeoSdk(ctx.getApplicationContext());
         iniFlurry(ctx.getApplicationContext());
+        iniBaidu(ctx);
     }
 
     /**
      * add an event that we will push to log service
      * 
+     * @param ctx activity context
      * @param level log level for leoSDK, can be LeoStat.P0 ~ LeoStat.P4
      * @param eventID event type of this event
      * @param eventDescription detail of this event
      */
-    public static void addEvent(int level, String eventID, String eventDescription) {
-        LeoStat.addEvent(level, eventID, eventDescription);
+    public static void addEvent(Context ctx, int level, String id, String description) {
+        // leo
+        LeoStat.addEvent(level, id, description);
+        // flurry
         Map<String, String> params = new HashMap<String, String>();
-        params.put("description", eventDescription);
-        FlurryAgent.logEvent(eventID, params);
+        params.put("description", description);
+        FlurryAgent.logEvent(id, params);
+        // baidu
+        StatService.onEvent(ctx, id, description);
     }
 
     public static void endSession(Context ctx) {
         LeoStat.endSession();
-        FlurryAgent.onEndSession(ctx);
     }
 
     private static void iniLeoSdk(Context ctx) {
@@ -57,7 +64,19 @@ public class SDKWrapper {
     private static void iniFlurry(Context ctx) {
         FlurryAgent.setVersionName(ctx.getString(R.string.version_name) + "_"
                 + ctx.getString(R.string.channel_code));
-        FlurryAgent.onStartSession(ctx, "F6PHG92TXG5QZ48H4YC8");
+        FlurryAgent.setLogEvents(true);
+        // TODO: disable internal log when release
+        FlurryAgent.setLogEnabled(true);
+    }
+
+    private static void iniBaidu(Context ctx) {
+        StatService.setAppKey("1004e462a2");
+        StatService.setAppChannel(ctx, ctx.getString(R.string.channel_code), true);
+        StatService.setSessionTimeOut(30);
+        StatService.setOn(ctx, StatService.EXCEPTION_LOG);
+        StatService.setSendLogStrategy(ctx, SendStrategyEnum.APP_START, 1, false);
+        StatService.setLogSenderDelayed(5);
+        StatService.setDebugOn(true);
     }
 
 }
