86dfb82b19054afbfd04820208e771dcc8fe0e2f
run <run@run-All-Series>
2014-11-15 17:23:16 +0800

    lockerTheme

--- src/com/leo/appmaster/model/AppLockerThemeBean.java
+++ src/com/leo/appmaster/model/AppLockerThemeBean.java
@@ -1,19 +1,23 @@
 package com.leo.appmaster.model;
 
+import android.graphics.drawable.Drawable;
+
 public class AppLockerThemeBean {
 		private String themeName;
-		private int themeImage;
+		private Drawable themeImage;
 		private String[] url;
 		private String packageName;
 		private String flagName;
-		public AppLockerThemeBean(String themeName, int themeImage,
-				String[] url, String packageName, String flagName) {
+		private String isVisibility;
+		public AppLockerThemeBean(String themeName, Drawable themeImage,
+				String[] url, String packageName, String flagName,String isVisibility) {
 			super();
 			this.themeName = themeName;
 			this.themeImage = themeImage;
 			this.url = url;
 			this.packageName = packageName;
 			this.flagName = flagName;
+			this.isVisibility=isVisibility;
 		}
 		public AppLockerThemeBean() {
 			super();
@@ -24,10 +28,10 @@ public class AppLockerThemeBean {
 		public void setThemeName(String themeName) {
 			this.themeName = themeName;
 		}
-		public int getThemeImage() {
+		public Drawable getThemeImage() {
 			return themeImage;
 		}
-		public void setThemeImage(int themeImage) {
+		public void setThemeImage(Drawable themeImage) {
 			this.themeImage = themeImage;
 		}
 		public String[] getUrl() {
@@ -47,6 +51,12 @@ public class AppLockerThemeBean {
 		}
 		public void setFlagName(String flagName) {
 			this.flagName = flagName;
+		}
+		public String getIsVisibility() {
+			return isVisibility;
+		}
+		public void setIsVisibility(String isVisibility) {
+			this.isVisibility = isVisibility;
 		}
 		
 		
