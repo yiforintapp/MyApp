dca963eb68524913c51637139b9967c576f37c50
run <run@run-All-Series>
2014-11-26 14:09:20 +0800

    aboutActivity add popmenu

--- src/com/leo/appmaster/appsetting/AboutActivity.java
+++ src/com/leo/appmaster/appsetting/AboutActivity.java
@@ -1,4 +1,6 @@
+
 package com.leo.appmaster.appsetting;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -35,13 +37,13 @@ import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.PopupWindow.OnDismissListener;
 
-public class AboutActivity extends BaseActivity implements OnClickListener{
+public class AboutActivity extends BaseActivity implements OnClickListener {
 
     private CommonTitleBar mTtileBar;
     private TextView mAppVersion;
     private LeoPopMenu mLeoPopMenu;
     private Button mShowProtocol;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,15 +53,15 @@ public class AboutActivity extends BaseActivity implements OnClickListener{
         mTtileBar.setOptionImageVisibility(View.VISIBLE);
         mTtileBar.openBackView();
         mTtileBar.setOptionText("");
-        mTtileBar.setOptionImage(R.drawable.setting_btn);
+        mTtileBar.setOptionImage(R.drawable.setting_selector);
         mTtileBar.setOptionListener(this);
         mShowProtocol = (Button) findViewById(R.id.check_update_button);
         mShowProtocol.setOnClickListener(this);
-        
-        mAppVersion = (TextView)findViewById(R.id.app_version);
+
+        mAppVersion = (TextView) findViewById(R.id.app_version);
         try {
-            String versionName = getPackageManager().getPackageInfo(getPackageName(),0).versionName;
-            mAppVersion.setText("V"+versionName);
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            mAppVersion.setText("V" + versionName);
         } catch (NameNotFoundException e) {
             e.printStackTrace();
         }
@@ -73,20 +75,21 @@ public class AboutActivity extends BaseActivity implements OnClickListener{
 
     @Override
     protected void onResume() {
-        
+
         if (mShowProtocol != null) {
             mShowProtocol.setText(R.string.protocolBar);
         }
-        
+
         super.onResume();
     }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.check_update_button:
-              Intent intent=new Intent();
-              intent.setClass(AboutActivity.this,ProtocolActivity.class);
-              startActivity(intent);
+                Intent intent = new Intent();
+                intent.setClass(AboutActivity.this, ProtocolActivity.class);
+                startActivity(intent);
                 break;
             case R.id.tv_option_image:
 
@@ -100,77 +103,63 @@ public class AboutActivity extends BaseActivity implements OnClickListener{
                         public void onItemClick(AdapterView<?> parent, View view,
                                 int position, long id) {
                             if (position == 0) {
-                                Uri uri=Uri.parse("https://plus.google.com/u/0/communities/112552044334117834440");                               
-                                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                                Uri uri = Uri
+                                        .parse("https://plus.google.com/u/0/communities/112552044334117834440");
+                                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                                 startActivity(intent);
                             } else if (position == 1) {
 
-                                /*if (AppUtil.appInstalled(getApplicationContext(),
-                                        "com.facebook.katana")) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                                    Uri uri = Uri
-                                            .parse("market://details?id=com.leo.appmaster&referrer=utm_source=AppMaster");
-                                    intent.setData(uri);
-
-                                            ComponentName cn = new ComponentName(
-                                                    "com.facebook.katana",
-                                                    "com.google.android.finsky.activities.MainActivity");
-                                    intent.setComponent(cn);
-                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    startActivity(intent);
-                                    mAppVersion.postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            Intent intent2 = new Intent(
-                                                    AboutActivity.this,
-                                                    GooglePlayGuideActivity.class);
-                                            intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                            startActivity(intent2);
-                                        }
-                                    }, 200);
-                                } else {*/
-                                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                                    Uri uri = Uri
-                                            .parse("https://www.facebook.com/pages/App-Master/1709302419294051");
-                                    intent.setData(uri);
-                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    startActivity(intent);
-//                                }
+                                /*
+                                 * if
+                                 * (AppUtil.appInstalled(getApplicationContext
+                                 * (), "com.facebook.katana")) { Intent intent =
+                                 * new Intent(Intent.ACTION_VIEW); Uri uri = Uri
+                                 * .parse(
+                                 * "market://details?id=com.leo.appmaster&referrer=utm_source=AppMaster"
+                                 * ); intent.setData(uri); ComponentName cn =
+                                 * new ComponentName( "com.facebook.katana",
+                                 * "com.google.android.finsky.activities.MainActivity"
+                                 * ); intent.setComponent(cn);
+                                 * intent.setFlags(Intent
+                                 * .FLAG_ACTIVITY_NEW_TASK);
+                                 * startActivity(intent);
+                                 * mAppVersion.postDelayed(new Runnable() {
+                                 * @Override public void run() { Intent intent2
+                                 * = new Intent( AboutActivity.this,
+                                 * GooglePlayGuideActivity.class);
+                                 * intent2.setFlags
+                                 * (Intent.FLAG_ACTIVITY_NEW_TASK);
+                                 * startActivity(intent2); } }, 200); } else {
+                                 */
+                                Intent intent = new Intent(Intent.ACTION_VIEW);
+                                Uri uri = Uri
+                                        .parse("https://www.facebook.com/pages/App-Master/1709302419294051");
+                                intent.setData(uri);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                // }
                             }
                             mLeoPopMenu.dismissSnapshotList();
                         }
                     });
                 }
                 mLeoPopMenu.setPopMenuItems(getPopMenuItems());
-                mLeoPopMenu.showPopMenu(this, mTtileBar.findViewById(R.id.tv_option_image), null,
-                        new OnDismissListener() {
-                            @Override
-                            public void onDismiss() {
-                                updateSettingIcon();
-                            }
-                        });
-
+                mLeoPopMenu.showPopMenu(this, mTtileBar.findViewById(R.id.tv_option_image), null,null);
                 break;
-
             default:
                 break;
         }
-        
-    }
 
+    }
+/**
+ * getPopMenuItems
+ * @return
+ */
     private List<String> getPopMenuItems() {
         List<String> listItems = new ArrayList<String>();
         Resources resources = AppMasterApplication.getInstance().getResources();
         listItems.add(resources.getString(R.string.about_group));
         listItems.add(resources.getString(R.string.about_praise));
         return listItems;
-    }
-
-    private void updateSettingIcon() {
-        if (LeoStat.isUpdateAvailable()) {
-            mTtileBar.setOptionImage(R.drawable.setting_updated_selector);
-        } else {
-            mTtileBar.setOptionImage(R.drawable.setting_selector);
-        }
     }
 }
