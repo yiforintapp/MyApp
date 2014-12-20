31e7a430ecf7f8dc2ce1f317c153ecbffbc85272
yangyun <lan@leoers.com>
2014-12-20 10:19:52 +0800

    Config and proguard

--- src/com/leo/appmaster/AppMasterConfig.java
+++ src/com/leo/appmaster/AppMasterConfig.java
@@ -16,8 +16,8 @@ public class AppMasterConfig {
 	/*
 	 * RECOMMEND URL
 	 */
-	public static final String APP_RECOMMEND_URL = "http://api.leostat.com/appmaster/apprecommend/list";
-	public static final String CHECK_NEW_BUSINESS_APP = "http://api.leostat.com/appmaster/apprecommend/checkappupdate";
+    public static final String APP_RECOMMEND_URL = "http://api.leostat.com/appmaster/apprecommend/list";
+    public static final String CHECK_NEW_BUSINESS_APP = "http://api.leostat.com/appmaster/apprecommend/checkappupdate";
 
 	public static final int TIME_2_HOUR = 2 * 60 * 60 * 1000;
 	public static final int TIME_12_HOUR = 12 * 60 * 60 * 1000;
@@ -36,8 +36,8 @@ public class AppMasterConfig {
 //	   /*
 //     * RECOMMEND URL
 //     */
-//    public static final String APP_RECOMMEND_URL = "http://192.168.1.201:8080/leo/appmaster/apprecommend/list";
-//    public static final String CHECK_NEW_BUSINESS_APP = "http://192.168.1.201:8080/leo/appmaster/apprecommend/checkappupdate";
+//    public static final String APP_RECOMMEND_URL = "http://api.leostat.com/appmaster/apprecommend/list";
+//    public static final String CHECK_NEW_BUSINESS_APP = "http://api.leostat.com/appmaster/apprecommend/checkappupdate";
 //    
 //    
 //    public static final int TIME_2_HOUR = 2 * 60 * 60 * 1000;
