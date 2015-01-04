d9f37b441048ee4bd6c0ce3ea557eed3deefafef
zhangwenyang <zhangwenyang@zhangwenyang-LEO-SZ>
2015-01-04 15:44:15 +0800

    refactor lock logic

--- src/com/leo/appmaster/applocker/logic/LockHandler.java
+++ src/com/leo/appmaster/applocker/logic/LockHandler.java
@@ -64,8 +64,8 @@ public class LockHandler extends BroadcastReceiver {
 
 		if (!pkg.equals(mLastRunningPkg)) {
 			String myPackage = mContext.getPackageName();
-			if (pkg.equals(myPackage)
-					|| (mLastRunningPkg.equals(myPackage) && mLastRuningActivity
+			if (/*pkg.equals(myPackage)
+					|| */(mLastRunningPkg.equals(myPackage) && mLastRuningActivity
 							.contains("LockScreenActivity"))) {
 				mLastRunningPkg = pkg;
 				mLastRuningActivity = activity;
