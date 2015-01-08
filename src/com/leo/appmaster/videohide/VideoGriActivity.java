644445f7768a20fb7b447f36664fd2a836e4fb20
yangyun <lan@leoers.com>
2015-01-08 19:11:10 +0800

    Fix crash from feedbacks

--- src/com/leo/appmaster/videohide/VideoGriActivity.java
+++ src/com/leo/appmaster/videohide/VideoGriActivity.java
@@ -586,7 +586,7 @@ public class VideoGriActivity extends BaseActivity implements OnItemClickListene
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (REQUEST_CODE == requestCode) {
+        if (REQUEST_CODE == requestCode && mVideoItems != null) {
             Bundle bundle = data.getExtras();
             if(bundle != null) {
                 ArrayList<String> resultPath = (ArrayList<String>) bundle.get("path");
