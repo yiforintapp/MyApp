6e67e52713d719ec953774506939ef26727f29f1
yangyun <lan@leoers.com>
2015-01-16 11:28:11 +0800

    change config

--- src/com/leo/appmaster/AppMasterConfig.java
+++ src/com/leo/appmaster/AppMasterConfig.java
@@ -8,33 +8,29 @@ import android.util.Log;
 public class AppMasterConfig {
 
 	/*-----    Debug configurations   ------ */
-	// public static final boolean LOGGABLE = true;
-	// public static final int SDK_LOG_LEVER = Log.DEBUG;
-	// /*
-	// * RECOMMEND URL
-	// */
-	// public static final String APP_RECOMMEND_URL =
-	// "http://api.leostat.com/appmaster/apprecommend/list";
-	// public static final String CHECK_NEW_BUSINESS_APP =
-	// "http://api.leostat.com/appmaster/apprecommend/checkappupdate";
-	// public static final int TIME_2_HOUR = 2 * 60 * 60 * 1000;
-	// public static final int TIME_12_HOUR = 12 * 60 * 60 * 1000;
-	// public static final int TIME_2_HOUR = 2 * 60 * 1000;
-	// public static final int TIME_12_HOUR = 2 * 60 * 1000;
-	/*-----    Debug configurations  end  ------ */
-	
-	/*-----     Release configurations   ------*/
-	public static final boolean LOGGABLE = true;
-
-/* change this to Log.ERROR when release */
-	public static final int SDK_LOG_LEVEL = Log.DEBUG;
-	/*
+	 public static final boolean LOGGABLE = true;
+	 public static final int SDK_LOG_LEVEL = Log.DEBUG;
+	 /*
 	 * RECOMMEND URL
 	 */
-	public static final String APP_RECOMMEND_URL = "http://192.168.1.201:8080/leo/appmaster/apprecommend/list";
-	public static final String CHECK_NEW_BUSINESS_APP = "http://api.leostat.com/appmaster/apprecommend/checkappupdate";
-	public static final int TIME_2_HOUR = 2 * 60 * 60 * 1000;
-	public static final int TIME_12_HOUR = 12 * 60 * 60 * 1000;
-	/*-----     Release configurations end   ------*/
+	 public static final String APP_RECOMMEND_URL = "http://api.leostat.com/appmaster/apprecommend/list";
+	 public static final String CHECK_NEW_BUSINESS_APP = "http://api.leostat.com/appmaster/apprecommend/checkappupdate";
+	 public static final int TIME_2_HOUR = 2 * 60 * 60 * 1000;
+	 public static final int TIME_12_HOUR = 12 * 60 * 60 * 1000;
+	/*-----    Debug configurations  end  ------ */
+	
+//	/*-----     Release configurations   ------*/
+//	public static final boolean LOGGABLE = false;
+//
+///* change this to Log.ERROR when release */
+//	public static final int SDK_LOG_LEVEL = Log.ERROR;
+//	/*
+//	 * RECOMMEND URL
+//	 */
+//	public static final String APP_RECOMMEND_URL = "http://api.leostat.com/appmaster/apprecommend/list";
+//	public static final String CHECK_NEW_BUSINESS_APP = "http://api.leostat.com/appmaster/apprecommend/checkappupdate";
+//	public static final int TIME_2_HOUR = 2 * 60 * 60 * 1000;
+//	public static final int TIME_12_HOUR = 12 * 60 * 60 * 1000;
+//	/*-----     Release configurations end   ------*/
 
 }
