ca5d38df3aeb590e7cce173340d3ab0f496647c5
run <you@example.com>
2015-03-30 17:09:52 +0800

    commit privacycontact

--- src/com/leo/appmaster/privacycontact/AddFromMessageListActivity.java
+++ src/com/leo/appmaster/privacycontact/AddFromMessageListActivity.java
@@ -4,6 +4,7 @@ package com.leo.appmaster.privacycontact;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
