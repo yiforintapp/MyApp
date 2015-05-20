9de95dc5491e064e292a5cf35b3677fd62f10faa
nicky <nicky@leoers.com>
2015-05-20 16:18:51 +0800

    asd

--- src/com/leo/appmaster/sdk/update/UIHelper.java
+++ src/com/leo/appmaster/sdk/update/UIHelper.java
@@ -244,7 +244,7 @@ public class UIHelper implements com.leo.analytics.update.IUIHelper {
             AppMasterApplication.getInstance().exitApplication();
         }
         if (ui_type == IUIHelper.TYPE_CHECK_NEED_UPDATE
-                && !isAppOnTop(mContext) || ui_type == IUIHelper.BACK_DOWNLOAD_DONE) {
+                && !isAppOnTop(mContext) ) {
             //|| ui_type == IUIHelper.BACK_DOWNLOAD_DONE
             sendUpdateNotification();
         } else {
