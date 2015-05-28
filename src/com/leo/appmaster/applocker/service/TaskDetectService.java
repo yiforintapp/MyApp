2daba2ce9350b63ac9d4bffc2988ee06b31346c2
run <you@example.com>
2015-05-28 09:47:13 +0800

    commit        quickgesture

--- src/com/leo/appmaster/applocker/service/TaskDetectService.java
+++ src/com/leo/appmaster/applocker/service/TaskDetectService.java
@@ -446,7 +446,7 @@ public class TaskDetectService extends Service {
                                     .updateFloatWindowBackgroudColor(FloatWindowHelper.mEditQuickAreaFlag);
                         }
                         if (isAppsAndHome) {
-                            if (!isRuningFreeDisturbApp(mActivityManager)) {
+                            if (!isRuningFreeDisturbApp(mActivityManager) || FloatWindowHelper.mEditQuickAreaFlag) {
                                 FloatWindowHelper.createFloatWindow(getApplicationContext(), value);
                             } else {
                                 FloatWindowHelper.removeAllFloatWindow(getApplicationContext());
