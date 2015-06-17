deb0eb94fb3fd32b2224419b8a9b04ed9fa3a2f8
run <you@example.com>
2015-06-17 20:30:17 +0800

    commit modify new user first tip count

---
+++ src/com/leo/appmaster/applocker/FamilyModeProxyActivity.java
@@ -0,0 +1,27 @@
+package com.leo.appmaster.applocker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.leo.appmaster.applocker.service.StatusBarEventService;
+import com.leo.appmaster.sdk.SDKWrapper;
+
+public class FamilyModeProxyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        boolean mQuickLockMode = intent.getBooleanExtra("quick_lock_mode", false);
+        String mQuickModeName = intent.getStringExtra("lock_mode_name");
+        int mQuiclModeId = intent.getIntExtra("lock_mode_id", -1);
+        Intent shortcutIntent = new Intent(this, LockScreenActivity.class);
+        shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        shortcutIntent.putExtra("quick_lock_mode", mQuickLockMode);
+        shortcutIntent.putExtra("lock_mode_id", mQuiclModeId);
+        shortcutIntent.putExtra("lock_mode_name", mQuickModeName);
+        startActivity(shortcutIntent);
+        finish();
+    }
+}
