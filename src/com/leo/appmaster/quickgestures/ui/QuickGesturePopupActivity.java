63e8a73959d43c7fd59c6e2b334f648bd6d24ef9
run <you@example.com>
2015-06-24 11:21:09 +0800

    commit sliding area modify

--- src/com/leo/appmaster/quickgestures/ui/QuickGesturePopupActivity.java
+++ src/com/leo/appmaster/quickgestures/ui/QuickGesturePopupActivity.java
@@ -57,6 +57,7 @@ public class QuickGesturePopupActivity extends BaseActivity {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         if (!hasFocus) {
+            createFloatView();
             FloatWindowHelper.mGestureShowing = false;
             mContainer.saveGestureType();
             finish();
@@ -109,8 +110,6 @@ public class QuickGesturePopupActivity extends BaseActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        FloatWindowHelper.createFloatWindow(getApplicationContext(),
-                QuickGestureManager.getInstance(getApplicationContext()).mSlidAreaSize);
     }
 
     private void fillDynamicLayout(boolean loadExtra) {
@@ -151,5 +150,10 @@ public class QuickGesturePopupActivity extends BaseActivity {
         overridePendingTransition(0, 0);
         super.finish();
     }
-
+    /*快捷手势消失，立即创建响应热区*/
+    private void createFloatView(){
+        FloatWindowHelper.createFloatWindow(getApplicationContext(),
+                QuickGestureManager.getInstance(getApplicationContext()).mSlidAreaSize);
+        QuickGestureManager.getInstance(this).startFloatWindow();
+    }
 }
