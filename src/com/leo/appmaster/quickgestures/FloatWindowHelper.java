bf0685831b7b75f56b0ee6dfc1f8f451ed85700e
run <you@example.com>
2015-06-26 20:34:36 +0800

    commit area color

--- src/com/leo/appmaster/quickgestures/FloatWindowHelper.java
+++ src/com/leo/appmaster/quickgestures/FloatWindowHelper.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.util.Log;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
