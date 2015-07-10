2a6d01a13fa85dd4f90b01d9459072a5a34c0eb7
chenfengshi <chenfengshi@leomaster.com>
2015-07-10 14:08:48 +0800

    适配

--- src/com/leo/appmaster/quickgestures/ui/QuickGestureActivity.java
+++ src/com/leo/appmaster/quickgestures/ui/QuickGestureActivity.java
@@ -2,6 +2,7 @@
 package com.leo.appmaster.quickgestures.ui;
 
 import java.util.List;
+import java.util.Locale;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -183,7 +184,15 @@ public class QuickGestureActivity extends BaseActivity implements OnTouchListene
         mGestureSwitch = (TextView) findViewById(R.id.gesture_switch_text);
         mGestureSwitch.setOnClickListener(this);
         mSlideAreaSetBtn = (Button) findViewById(R.id.slide_setting_button);
-
+        
+        Locale locale =getResources().getConfiguration().locale;
+        String language = locale.getLanguage();
+        //俄文环境的适配
+        if(language.endsWith("ru"))
+        {
+            mSlideAreaSetBtn.setTextSize(11);
+        }
+        
         mSlideGuideView = (RelativeLayout) findViewById(R.id.slide_guide_show);
         mSlideGuideView.setOnClickListener(this);
         mSlideGuidehand = (ImageView) mSlideGuideView.findViewById(R.id.gesture_arrow_hand);
