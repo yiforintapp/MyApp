b917881a2822dfc52c7e9ab24e175d19ccd7e759
chenfengshi <chenfengshi@leomaster.com>
2015-07-15 19:42:33 +0800

    aa

--- src/com/leo/appmaster/applocker/BeautyWeiZhuang.java
+++ src/com/leo/appmaster/applocker/BeautyWeiZhuang.java
@@ -1,18 +1,65 @@
+
 package com.leo.appmaster.applocker;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.leo.appmaster.AppMasterPreference;
 import com.leo.appmaster.R;
 import com.leo.appmaster.sdk.BaseActivity;
+import com.leo.appmaster.ui.dialog.LEOAlarmDialog;
+import com.leo.appmaster.ui.dialog.LEOAlarmDialog.OnDiaogClickListener;
 
-public class BeautyWeiZhuang extends BaseActivity 
+public class BeautyWeiZhuang extends BaseActivity
 {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            // TODO Auto-generated method stub
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_app_beauty);
-            
-            
+    private final static int BEAUTYWEIZHUANG = 0;
+    private LEOAlarmDialog mAlarmDialog;
+    private AppMasterPreference mAppMasterSP;
+    
+    
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_beauty);
+        init();
+    }
+
+    private void init() {
+        mAppMasterSP=AppMasterPreference.getInstance(this);
+        
+    }
+    
+    public void click(View v)
+    {
+        showAlarmDialog("标题啦","呵呵","确定");
+    }
+    
+
+    private void showAlarmDialog(String title, String content, String sureText)
+    {
+        if (mAlarmDialog == null)
+        {
+            mAlarmDialog = new LEOAlarmDialog(this);
+            mAlarmDialog.setOnClickListener(new OnDiaogClickListener() {
+                @Override
+                public void onClick(int which) {
+                    // ok
+                    if (which == 1)
+                    {
+                        
+                        mAppMasterSP.setPretendLock(BEAUTYWEIZHUANG);
+                       
+                        BeautyWeiZhuang.this.finish();
+                    }
+
+                }
+            });
         }
+        mAlarmDialog.setSureButtonText(sureText);
+        mAlarmDialog.setTitle(title);
+        mAlarmDialog.setContent(content);
+        mAlarmDialog.show();
+    }
 }
