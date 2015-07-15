263b6701cbfde500516b01b0742e0851b33d4791
chenfengshi <chenfengshi@leomaster.com>
2015-07-15 17:38:34 +0800

    新增美女伪装入口

---
+++ src/com/leo/appmaster/applocker/BeautyWeiZhuang.java
@@ -0,0 +1,18 @@
+package com.leo.appmaster.applocker;
+
+import android.os.Bundle;
+
+import com.leo.appmaster.R;
+import com.leo.appmaster.sdk.BaseActivity;
+
+public class BeautyWeiZhuang extends BaseActivity 
+{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            // TODO Auto-generated method stub
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_app_beauty);
+            
+            
+        }
+}
