2f1798d5673cf6ddae9ab290f447551b33f68cff
yangyun <yangyun@leomaster.com>
2015-07-20 19:40:33 +0800

    optimize text compute

--- src/com/leo/appmaster/privacy/PrivacyLevelView.java
+++ src/com/leo/appmaster/privacy/PrivacyLevelView.java
@@ -186,7 +186,7 @@ public class PrivacyLevelView extends View {
         mScanningIconEndRect.top = mScanningIconCenterRect.top;
         mScanningIconEndRect.bottom =  mScanningIconCenterRect.top + scanningIconSize;
         
-        if(!mLastIconDrawBount.equals(mIconDrawBount) && getVisibility() == View.VISIBLE) {
+        if(!mLastIconDrawBount.equals(mIconDrawBount) && isShown()) {
             int maxTextWidth = (int)(drawW * SMALL_TEXT_WIDTH_PERCENT);
             int textSize = computeTextSize(mLevelText, mSmallTextSize, maxTextWidth, mPaint);
             mRealSmallTextSize = (int)mPaint.getTextSize();
