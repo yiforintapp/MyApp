7a977cc5bdcbe16c92676c26efc8489c14338e23
chenfengshi <chenfengshi@leomaster.com>
2015-07-24 15:02:21 +0800

    压缩图片资源

--- src/com/leo/appmaster/applocker/BeautyWeiZhuang.java
+++ src/com/leo/appmaster/applocker/BeautyWeiZhuang.java
@@ -42,8 +42,7 @@ public class BeautyWeiZhuang extends BaseActivity
         });
         
     }
-    
-    
+        
     private void showAlarmDialog(String title, String content, String sureText)
     {
         if (mAlarmDialog == null)
