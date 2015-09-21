c67a5c11baed0d109eb92af935e2e3e37a445cb3
lishuai <lishuai@leomaster.com>
2015-09-21 17:15:22 +0800

    本地测试图片修改回去

--- src/com/leo/appmaster/msgcenter/MsgCenterAdapter.java
+++ src/com/leo/appmaster/msgcenter/MsgCenterAdapter.java
@@ -177,9 +177,9 @@ public class MsgCenterAdapter extends BaseAdapter {
             holder.description.setPadding(0, mDesPadding, 0, mDesPadding);
 //            holder.image.setPadding(0, 0, 0, 0);
         }
-        String url = "file://" + Environment.getExternalStorageDirectory() + "/banner.png";
-//        ImageLoader.getInstance().displayImage(msg.imageUrl, holder.image, commonOption);
-        ImageLoader.getInstance().displayImage(url, holder.image, commonOption);
+//        String url = "file://" + Environment.getExternalStorageDirectory() + "/banner.png";
+//        ImageLoader.getInstance().displayImage(url, holder.image, commonOption);
+        ImageLoader.getInstance().displayImage(msg.imageUrl, holder.image, commonOption);
 
         return convertView;
     }
