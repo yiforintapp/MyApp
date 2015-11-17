e03da731d8845c37b1628568a460ac22ebcc3a7f
yifulin <yifulin@leoers.com>
2015-11-17 21:20:29 +0800

    更改动画细节

--- src/com/leo/appmaster/home/PrivacyConfirmFragment.java
+++ src/com/leo/appmaster/home/PrivacyConfirmFragment.java
@@ -37,7 +37,9 @@ import com.leo.appmaster.privacycontact.MessageCallLogBean;
 import com.leo.appmaster.sdk.SDKWrapper;
 import com.leo.appmaster.ui.RippleView;
 import com.leo.appmaster.ui.dialog.LEOAlarmDialog;
+import com.leo.appmaster.utils.DipPixelUtil;
 import com.leo.appmaster.utils.LeoLog;
+import com.leo.appmaster.utils.Utilities;
 import com.leo.appmaster.wifiSecurity.WifiSecurityActivity;
 import com.leo.imageloader.DisplayImageOptions;
 import com.leo.imageloader.ImageLoader;
@@ -103,6 +105,7 @@ public class PrivacyConfirmFragment extends Fragment implements RippleView.OnRip
     private ImageView mThreeStar;
     private ImageView mFourStar;
     private ImageView mFiveStar;
+    private ImageView mGradeGesture;
 
     private CheckBox mSelectAllCb;
     private List<View> mContactViews;
@@ -617,6 +620,7 @@ public class PrivacyConfirmFragment extends Fragment implements RippleView.OnRip
         mThreeStar = (ImageView) include.findViewById(R.id.three_star);
         mFourStar = (ImageView) include.findViewById(R.id.four_star);
         mFiveStar = (ImageView) include.findViewById(R.id.five_star);
+        mGradeGesture = (ImageView) include.findViewById(R.id.grade_gesture);
     }
 
     /***
@@ -629,9 +633,49 @@ public class PrivacyConfirmFragment extends Fragment implements RippleView.OnRip
         ObjectAnimator fourStar = getObjectAnimator(mFourStar);
         ObjectAnimator fiveStar = getObjectAnimator(mFiveStar);
 
+        float currentY = mGradeGesture.getTop();
+        ObjectAnimator gestureMoveIn = ObjectAnimator.ofFloat(mGradeGesture,
+                "translationY", currentY + DipPixelUtil.dip2px(mActivity, 16), currentY);
+        gestureMoveIn.setDuration(2000);
+
+        ObjectAnimator gestureMoveOut =  ObjectAnimator.ofFloat(mGradeGesture,
+                "translationY",currentY , currentY + DipPixelUtil.dip2px(mActivity, 16));
+
+        ObjectAnimator gestureHide = ObjectAnimator.ofFloat(mGradeGesture, "alpha", 1f, 0f);
+
+        gestureMoveOut.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                mGradeGesture.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        AnimatorSet gestureAnimator = new AnimatorSet();
+        gestureAnimator.playTogether(gestureMoveOut, gestureHide);
+        gestureAnimator.setDuration(1000);
+
+
+
+        AnimatorSet starAnimator = new AnimatorSet();
+        starAnimator.playSequentially(oneStar, twoStar, threeStar, fourStar, fiveStar);
+
+        ObjectAnimator emptyObjectAnimator = ObjectAnimator.ofFloat(mFiveStar, "alpha", 1f, 1f);
+        emptyObjectAnimator.setDuration(1000);
+
         final AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playSequentially(oneStar, twoStar, threeStar, fourStar, fiveStar);
+        animatorSet.play(gestureMoveIn).before(starAnimator);
+        animatorSet.play(starAnimator).with(gestureAnimator);
+        animatorSet.play(emptyObjectAnimator).after(starAnimator);
+
         animatorSet.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                mGradeGesture.setVisibility(View.VISIBLE);
+            }
+
             @Override
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
@@ -672,7 +716,7 @@ public class PrivacyConfirmFragment extends Fragment implements RippleView.OnRip
     private ObjectAnimator getObjectAnimator(ImageView img) {
 
         ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(img, "alpha", 0f, 1f);
-        objectAnimator.setDuration(1000);
+        objectAnimator.setDuration(250);
         objectAnimator.addListener(new ObjectAnimStartListener(img));
 
         return  objectAnimator;
