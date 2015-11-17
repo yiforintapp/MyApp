0574e2d059a9336020854f177747e1b1ea4584e7
yifulin <yifulin@leoers.com>
2015-11-17 18:18:28 +0800

    添加五星好评动画

--- src/com/leo/appmaster/home/PrivacyConfirmFragment.java
+++ src/com/leo/appmaster/home/PrivacyConfirmFragment.java
@@ -43,6 +43,10 @@ import com.leo.imageloader.DisplayImageOptions;
 import com.leo.imageloader.ImageLoader;
 import com.leo.imageloader.core.FailReason;
 import com.leo.imageloader.core.ImageLoadingListener;
+import com.leo.tools.animator.Animator;
+import com.leo.tools.animator.AnimatorListenerAdapter;
+import com.leo.tools.animator.AnimatorSet;
+import com.leo.tools.animator.ObjectAnimator;
 import com.mobvista.sdk.m.core.entity.Campaign;
 
 import java.lang.ref.WeakReference;
@@ -92,6 +96,13 @@ public class PrivacyConfirmFragment extends Fragment implements RippleView.OnRip
     private View mContactBtnDiv;
     private LinearLayout mContactContainor;
     private ImageView mContactArrowIv;
+
+    /** 评分星星 */
+    private ImageView mOneStar;
+    private ImageView mTwoStar;
+    private ImageView mThreeStar;
+    private ImageView mFourStar;
+    private ImageView mFiveStar;
 
     private CheckBox mSelectAllCb;
     private List<View> mContactViews;
@@ -177,9 +188,11 @@ public class PrivacyConfirmFragment extends Fragment implements RippleView.OnRip
         initWifiLayout(view);
 
         initContactLayout(view);
+        initGradeLayout(view);
 
         loadAd(view);
         mActivity.resetToolbarColor();
+        showStarAnimation();
     }
 
 
@@ -594,6 +607,75 @@ public class PrivacyConfirmFragment extends Fragment implements RippleView.OnRip
 
         String text = getActivity().getString(R.string.pri_pro_wifi_btn);
         mWifiBtnTv.setText(text);
+    }
+
+    private void initGradeLayout(View view) {
+        View include = view.findViewById(R.id.grade_security);
+
+        mOneStar = (ImageView) include.findViewById(R.id.one_star);
+        mTwoStar = (ImageView) include.findViewById(R.id.two_star);
+        mThreeStar = (ImageView) include.findViewById(R.id.three_star);
+        mFourStar = (ImageView) include.findViewById(R.id.four_star);
+        mFiveStar = (ImageView) include.findViewById(R.id.five_star);
+    }
+
+    /***
+     * 开始动画
+     */
+    private void showStarAnimation() {
+        ObjectAnimator oneStar = getObjectAnimator(mOneStar);
+        ObjectAnimator twoStar = getObjectAnimator(mTwoStar);
+        ObjectAnimator threeStar = getObjectAnimator(mThreeStar);
+        ObjectAnimator fourStar = getObjectAnimator(mFourStar);
+        ObjectAnimator fiveStar = getObjectAnimator(mFiveStar);
+
+        final AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(oneStar, twoStar, threeStar, fourStar, fiveStar);
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                changeStarInvisible();
+                animatorSet.start();
+            }
+        });
+        animatorSet.start();
+    }
+
+    /**每个星星动画开始监听*/
+    private class ObjectAnimStartListener extends AnimatorListenerAdapter {
+
+        private ImageView theView;
+
+        public ObjectAnimStartListener (ImageView imageView) {
+            this.theView = imageView;
+        }
+
+        @Override
+        public void onAnimationStart(Animator animation) {
+            super.onAnimationStart(animation);
+            theView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /***
+     * 设置所有星星不可见
+     */
+    private void changeStarInvisible() {
+        mOneStar.setVisibility(View.INVISIBLE);
+        mTwoStar.setVisibility(View.INVISIBLE);
+        mThreeStar.setVisibility(View.INVISIBLE);
+        mFourStar.setVisibility(View.INVISIBLE);
+        mFiveStar.setVisibility(View.INVISIBLE);
+    }
+
+    private ObjectAnimator getObjectAnimator(ImageView img) {
+
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(img, "alpha", 0f, 1f);
+        objectAnimator.setDuration(1000);
+        objectAnimator.addListener(new ObjectAnimStartListener(img));
+
+        return  objectAnimator;
     }
 
     @Override
