8a32597260a38757fb5ed0c52129a16509760f1c
chenfengshi <chenfengshi@leomaster.com>
2015-11-25 15:01:58 +0800

    用户反馈的问题类型增加旧选项

--- src/com/leo/appmaster/feedback/FeedbackActivity.java
+++ src/com/leo/appmaster/feedback/FeedbackActivity.java
@@ -58,7 +58,8 @@ public class FeedbackActivity extends BaseActivity implements OnClickListener,
 
     private final static int[] sCategoryIds = {
             R.string.home_tab_wifi, R.string.home_tab_lost,
-            R.string.home_tab_instruder,R.string.privacy_scan, R.string.category_other
+            R.string.home_tab_instruder,R.string.privacy_scan, R.string.category_lock, R.string.pravicy_protect,
+            R.string.app_manager, R.string.category_other,
     };
 
     private final ArrayList<String> mCategories = new ArrayList<String>();
