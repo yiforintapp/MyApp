904caf19380b58a6f68ad4b7440a0b97b043fc04
yangyun <yangyun@leomaster.com>
2015-11-27 14:12:24 +0800

    Remove unused code

--- src/com/leo/appmaster/ui/LeoGridBaseAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.leo.appmaster.ui;
-
-public interface LeoGridBaseAdapter {
-	public void reorderItems(int oldPosition, int newPosition);
-
-	public void setHideItem(int hidePosition);
-
-	public void removeItem(int position);
-}
