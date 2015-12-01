4ef5b65d40158a5aa3396c2a0b4d13eecb97d82b
lilongfei <run@leomaster.com>
2015-12-01 13:13:01 +0800

    320*240适配

--- src/com/leo/appmaster/utils/AppUtil.java
+++ src/com/leo/appmaster/utils/AppUtil.java
@@ -372,8 +372,7 @@ public class AppUtil {
             /*计算出实际宽高和目标宽高的比率*/
             final float heightRatio = (float) height / (float) reqHeight;
             final float widthRatio = (float) width / (float) reqWidth;
-            /*选择宽和高中最小的比率作为inSampleSize的值，这样可以保证最终图片的宽和高 一定都会大于等于目标的宽和高。*/
-            float tempInSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+            float tempInSampleSize = heightRatio > widthRatio ? heightRatio : widthRatio;
             if (tempInSampleSize < 2) {
                 inSampleSize = 1;
             } else {
