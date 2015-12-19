fa32fee44d4426c6822ce65551db30451f96fa5d
chenfengshi <chenfengshi@leomaster.com>
2015-12-19 16:19:49 +0800

    对话框2

--- src/com/leo/appmaster/callfilter/CallFIlterUIHelper.java
+++ src/com/leo/appmaster/callfilter/CallFIlterUIHelper.java
@@ -1,11 +1,16 @@
 package com.leo.appmaster.callfilter;
 
+import java.util.ArrayList;
+
 import com.leo.appmaster.R;
 import com.leo.appmaster.ui.dialog.LEOChoiceDialog;
 import com.leo.appmaster.ui.dialog.LEOWithSIngleCheckboxDialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class CallFIlterUIHelper {
 //    private static LEOWithSIngleCheckboxDialog mConfirmRemoveFromBlacklistDialog;
@@ -22,9 +27,15 @@ public class CallFIlterUIHelper {
         return dialog;
     }
     
-    public LEOChoiceDialog getCallHandleDialog() {
+    public LEOChoiceDialog getCallHandleDialog(String title) {
         LEOChoiceDialog dialog = new LEOChoiceDialog(mContext);
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("删除拦截记录");
+        list.add("移出黑名单");
+        list.add("标记");
         dialog.setNeedCheckbox(false);
+        dialog.setTitle(title);
+        dialog.setItemsWithDefaultStyle(list, 0);
         return dialog;
     }
 
