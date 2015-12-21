4eafebdc5fa21990961909346c543d5d06a643f5
chenfengshi <chenfengshi@leomaster.com>
2015-12-21 16:20:52 +0800

    拦截记录的activity

---
+++ src/com/leo/appmaster/callfilter/CallFilterRecordActivity.java
@@ -0,0 +1,14 @@
+package com.leo.appmaster.callfilter;
+
+import android.os.Bundle;
+
+import com.leo.appmaster.R;
+import com.leo.appmaster.sdk.BaseActivity;
+
+public class CallFilterRecordActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_callfilter_record);
+    }
+}
