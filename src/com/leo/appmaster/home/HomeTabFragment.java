7006ecba9104ff30ba6f0af63c31007ed7b4fcca
chenfengshi <chenfengshi@leomaster.com>
2015-12-30 21:05:16 +0800

    fix bug

--- src/com/leo/appmaster/home/HomeTabFragment.java
+++ src/com/leo/appmaster/home/HomeTabFragment.java
@@ -60,7 +60,7 @@ import java.util.List;
  */
 public class HomeTabFragment extends Fragment implements View.OnClickListener {
     private static final String TAG = "HomeTabFragment";
-    private static final boolean DBG = false;
+    private static final boolean DBG = true;
 
     private ImageView mRedDot;
     // 首页4个tab
@@ -343,21 +343,21 @@ public class HomeTabFragment extends Fragment implements View.OnClickListener {
                     SDKWrapper.addEvent(getActivity(), SDKWrapper.P1, "home", "home_theft");
                     startPhoneSecurity();
                     if (DBG) {
-//                        int[] pix = AppUtil.getScreenPix(getActivity());
-//                        LeoLog.i(TAG, "X=" + pix[0] + ",Y=" + pix[1]);
-//                        
-//                        CallFilterContextManagerImpl pm = (CallFilterContextManagerImpl) MgrContext.getManager(MgrContext.MGR_CALL_FILTER);
-//                        pm.setFilterUserNumber(50000);
-//                        pm.setFilterTipFroUser(3000);
-//                        pm.setSerBlackTipNum(3000);
-//                        pm.setSerMarkTipNum(50);
-//                        BlackListInfo info = new BlackListInfo();
-//                        info.setNumber("17097219182");
-//                        info.setAddBlackNumber(2258);
-//                        info.setMarkerType(0);
-//                        info.setMarkerNumber(50000);
-//                        CallFilterManager cm = CallFilterManager.getInstance(AppMasterApplication.getInstance());
-//                        cm.addFilterFroParse(info);
+                        int[] pix = AppUtil.getScreenPix(getActivity());
+                        LeoLog.i(TAG, "X=" + pix[0] + ",Y=" + pix[1]);
+                        
+                        CallFilterContextManagerImpl pm = (CallFilterContextManagerImpl) MgrContext.getManager(MgrContext.MGR_CALL_FILTER);
+                        pm.setFilterUserNumber(50000);
+                        pm.setFilterTipFroUser(3000);
+                        pm.setSerBlackTipNum(3000);
+                        pm.setSerMarkTipNum(50);
+                        BlackListInfo info = new BlackListInfo();
+                        info.setNumber("13632840685");
+                        info.setAddBlackNumber(2258);
+                        info.setMarkerType(0);
+                        info.setMarkerNumber(50000);
+                        CallFilterManager cm = CallFilterManager.getInstance(AppMasterApplication.getInstance());
+                        cm.addFilterFroParse(info);
                         Intent intent2 = new Intent(getActivity(), TestDemo.class);
                         startActivity(intent2);
 //
