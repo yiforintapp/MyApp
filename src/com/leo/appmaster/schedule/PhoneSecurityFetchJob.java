b3959508cabad02a654aa3df07062485b1d2ad8a
yifulin <yifulin@leoers.com>
2016-01-15 18:12:16 +0800

    获取骚扰拦截分享出发次数

--- src/com/leo/appmaster/schedule/PhoneSecurityFetchJob.java
+++ src/com/leo/appmaster/schedule/PhoneSecurityFetchJob.java
@@ -1,18 +1,20 @@
 package com.leo.appmaster.schedule;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.content.Context;
 
 import com.android.volley.VolleyError;
 import com.leo.appmaster.AppMasterApplication;
 import com.leo.appmaster.HttpRequestAgent;
+import com.leo.appmaster.db.PreferenceTable;
 import com.leo.appmaster.mgr.BatteryManager;
 import com.leo.appmaster.mgr.MgrContext;
 import com.leo.appmaster.mgr.impl.LostSecurityManagerImpl;
 import com.leo.appmaster.utils.LeoLog;
 import com.leo.appmaster.utils.NetWorkUtil;
+import com.leo.appmaster.utils.PrefConst;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by runlee on 15-10-23.
@@ -57,6 +59,15 @@ public class PhoneSecurityFetchJob extends FetchScheduleJob {
                 int appNumber = resp.getInt(BatteryManager.APP_THRESHOLD_KEY);
                 BatteryManager bm = (BatteryManager) MgrContext.getManager(MgrContext.MGR_APPLOCKER);
                 bm.setAppThreshold(appNumber);
+            }
+
+            boolean isCallFilterShareTimesNull = resp.isNull("harass_intercept_num"); // 判断key是否存在
+            PreferenceTable preferenceTable = PreferenceTable.getInstance();
+            if (!isCallFilterShareTimesNull) {
+                int callFilterTimes = resp.getInt("harass_intercept_num");
+                preferenceTable.putInt(PrefConst.KEY_CALL_FILTER_SHARE_TIMES, callFilterTimes);
+            } else {
+                preferenceTable.putInt(PrefConst.KEY_CALL_FILTER_SHARE_TIMES, 10); // 获取不到默认10次
             }
 
         } catch (JSONException e) {
