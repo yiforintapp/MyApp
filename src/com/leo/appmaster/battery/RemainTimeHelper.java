cba539c508fb6eef053d3d4befd3c003269c6c3d
StoneLam <stonelam@leoers.com>
2016-01-16 15:37:39 +0800

    增加统计充电时间的接口，用于训练充电时间，发布时需要去掉

--- src/com/leo/appmaster/battery/RemainTimeHelper.java
+++ src/com/leo/appmaster/battery/RemainTimeHelper.java
@@ -1,9 +1,20 @@
 package com.leo.appmaster.battery;
 
+import android.os.Build;
+import android.os.Environment;
+
 import com.leo.appmaster.db.PreferenceTable;
 import com.leo.appmaster.utils.LeoLog;
+import com.leo.imageloader.utils.IoUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 /**
@@ -42,15 +53,24 @@ public class RemainTimeHelper {
     private int mScale;
 
     static class PeriodUnit{
+        private static final String SEPERATOR = ":";
         // 充电到下一level的时间，单位为秒
         public int second;
         // 矫正次数
         public int count = 0;
-        public String pack(){
-            return second+","+count;
+        public PeriodUnit() {}
+        public PeriodUnit(int second, int count) {
+            this.second = second;
+            this.count = count;
+        }
+        public String toString(){
+            return second+SEPERATOR+count;
         }
         public static PeriodUnit fromString(String data){
-            String[] elements = data.split(",");
+            String[] elements = data.split(SEPERATOR);
+            if (elements.length != 2) {
+                return null;
+            }
             PeriodUnit pu = new PeriodUnit();
             try {
                 pu.second = Integer.parseInt(elements[0]);
@@ -79,6 +99,11 @@ public class RemainTimeHelper {
             } else {
                 pu = PeriodUnit.fromString(data);
             }
+            if (pu == null) {
+                // 保存的记录损坏
+                pu = new PeriodUnit(INITIAL_DATA[i], 1);
+                PreferenceTable.getInstance().putString(RT_PREFERENCE_KEY + i, pu.toString());
+            }
             mPreferenceList.add(pu);
         }
     }
@@ -88,7 +113,10 @@ public class RemainTimeHelper {
             PeriodUnit pu = mPreferenceList.get(level);
             pu.second = (pu.second*pu.count+period)/(pu.count+1);
             pu.count += 1;
-            PreferenceTable.getInstance().putString(RT_PREFERENCE_KEY+level, pu.pack());
+            PreferenceTable.getInstance().putString(RT_PREFERENCE_KEY+level, pu.toString());
+
+            // debug
+            dumpToDisk();
         }
     }
 
@@ -139,6 +167,36 @@ public class RemainTimeHelper {
             return time;
         } else {
             return (mScale-level)*mPreferenceList.get(0).second;
+        }
+    }
+
+    /* 用于保存校准数据 */
+    private void dumpToDisk(){
+        File picDir = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath());
+        File dir = new File(picDir.getPath() + File.separator + "leo_battery_charging");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File file = new File(dir + File.separator + "battery_charge_data.txt");
+        OutputStreamWriter writer = null;
+        try {
+            LeoLog.d(TAG, "dump to: " + file.getAbsolutePath());
+            writer = new OutputStreamWriter(new FileOutputStream(file, false));
+            writer.write(Build.MODEL+"\n");
+            writer.write(new SimpleDateFormat("yyyyMMddHHmmss").format(new Date())+"\n");
+            StringBuilder sb = new StringBuilder();
+            int size = mPreferenceList.size();
+            for (int i=0;i<size;i++) {
+                sb.append(mPreferenceList.get(i).toString()+", ");
+            }
+            writer.write(sb.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            IoUtils.closeSilently(writer);
         }
     }
 
