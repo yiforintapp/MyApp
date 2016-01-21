c07d2e0c38aae86b8dee198a1a1774e352af3d8d
StoneLam <stonelam@leoers.com>
2016-01-21 13:59:07 +0800

    大伟要求打开大陆开关用于测试

--- src/com/leo/appmaster/AppMasterConfig.java
+++ src/com/leo/appmaster/AppMasterConfig.java
@@ -26,7 +26,7 @@ public class AppMasterConfig {
 //    public static final long TRAFFIC_INTERNAL = 60 * 1000;
 
     // 区分国内外渠道
-    public static final boolean IS_FOR_MAINLAND_CHINA = false;
+    public static final boolean IS_FOR_MAINLAND_CHINA = true; // stone_debug 大伟要求打开，用于测试
 
     /*-----    Debug configurations  end  ------ */
 
