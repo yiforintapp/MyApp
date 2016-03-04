6a39cb756a7d4fa5b4cff420b5727ec4786a2ee3
nicky <nicky@leoers.com>
2016-03-04 11:03:41 +0800

    fix bug : AM-3988

--- src/com/leo/appmaster/battery/BatteryViewFragment.java
+++ src/com/leo/appmaster/battery/BatteryViewFragment.java
@@ -2191,7 +2191,7 @@ public class BatteryViewFragment extends BaseFragment implements View.OnTouchLis
         boolean isUrlEmpty = isGpUrlEmpty && isBrowserUrlEmpty; //判断两个地址是否都为空
 
         if (!isContentEmpty && !isImgUrlEmpty && !isTypeEmpty && !isUrlEmpty) {
-//        if (false) {
+//        if (true) {
             mSwiftyView = viewStub.inflate();
 
             mSwiftyImg = (ImageView) mSwiftyView.findViewById(R.id.card_img);
@@ -2259,7 +2259,7 @@ public class BatteryViewFragment extends BaseFragment implements View.OnTouchLis
         boolean isUrlEmpty = isGpUrlEmpty && isBrowserUrlEmpty; //判断两个地址是否都为空
 
         if (!isContentEmpty && !isImgUrlEmpty && !isTypeEmpty && !isUrlEmpty) {
-//        if (false) {
+//        if (true) {
             mExtraView = viewStub.inflate();
             mExtraTitle = (TextView) mExtraView.findViewById(R.id.card_title);
             mExtraImg = (ImageView) mExtraView.findViewById(R.id.card_img);
