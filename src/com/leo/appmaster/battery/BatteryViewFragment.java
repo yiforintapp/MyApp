35ecbce09a578f250753a9964e293ed22d27d16d
lishuai <lishuai@leomaster.com>
2016-03-07 19:48:24 +0800

    屏保动画打点

--- src/com/leo/appmaster/battery/BatteryViewFragment.java
+++ src/com/leo/appmaster/battery/BatteryViewFragment.java
@@ -443,6 +443,7 @@ public class BatteryViewFragment extends BaseFragment implements View.OnTouchLis
         BatteryManager btrManager = (BatteryManager) MgrContext.getManager(MgrContext.MGR_BATTERY);
         boolean isBatteryPowSavOpen = btrManager.getBatteryPowSavStatus();
         if (PrefTableHelper.shouldBatteryBoost() && isBatteryPowSavOpen) {
+            SDKWrapper.addEvent(mActivity, SDKWrapper.P1, "batterypage", "screen_save");
             ViewStub viewStub = (ViewStub) findViewById(R.id.boost_stub);
             mBoostView = (BatteryBoostController) viewStub.inflate();
 
