d14f5d04e114072be918c09318e3878e11c7e93c
Administrator <lilongfei@leoers.com>
2016-03-08 10:48:28 +0800

    关闭测试log

--- src/com/leo/appmaster/schedule/BlackConfigFetchJob.java
+++ src/com/leo/appmaster/schedule/BlackConfigFetchJob.java
@@ -20,7 +20,7 @@ import org.json.JSONObject;
  */
 public class BlackConfigFetchJob extends FetchScheduleJob {
 
-    public static final boolean DBG = true;
+    public static final boolean DBG = false;
 
     /**
      * 骚扰拦截用户数量参数
