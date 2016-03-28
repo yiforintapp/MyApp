68fec4cf093d87d9988a62f12ce451fe3b22c21a
chenfengshi <chenfengshi@leomaster.com>
2016-03-28 19:05:47 +0800

    全部设备管理器权限声明

--- src/com/leo/appmaster/home/HomeMoreActivity.java
+++ src/com/leo/appmaster/home/HomeMoreActivity.java
@@ -1,11 +1,14 @@
 package com.leo.appmaster.home;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.leo.appmaster.R;
 import com.leo.appmaster.battery.BatterySettingActivity;
+import com.leo.appmaster.db.PreferenceTable;
 import com.leo.appmaster.intruderprotection.IntruderprotectionActivity;
 import com.leo.appmaster.mgr.MgrContext;
 import com.leo.appmaster.mgr.impl.LostSecurityManagerImpl;
@@ -15,6 +18,7 @@ import com.leo.appmaster.phoneSecurity.PhoneSecurityGuideActivity;
 import com.leo.appmaster.sdk.BaseActivity;
 import com.leo.appmaster.ui.CommonToolbar;
 import com.leo.appmaster.ui.RippleView;
+import com.leo.appmaster.utils.PrefConst;
 
 /**
  * Created by chenfs on 16-3-28.
@@ -26,11 +30,38 @@ public class HomeMoreActivity extends BaseActivity implements View.OnClickListen
     private RippleView mRvBatteryScreenEntry;
     private RippleView mRvSettingEntry;
 
+    private RippleView mRvCallfilterEntry;
+    private RippleView mRvWifiEntry;
+    private RippleView mRvFlowEntry;
+    private RippleView mRvBatteryEntry;
+
+    private RelativeLayout mRlCallFilter;
+    private RelativeLayout mRlWifi;
+    private RelativeLayout mRlFlowManagement;
+    private RelativeLayout mRlBatteryManagement;
+
+    private PreferenceTable mPt;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_more);
         initUI();
+        mPt = PreferenceTable.getInstance();
+        tryShowOldEntry();
+    }
+
+    private void tryShowOldEntry() {
+        if (!mPt.getBoolean(PrefConst.KEY_IS_OLD_USER, false)) {
+            mRlCallFilter.setVisibility(View.GONE);
+            mRlWifi.setVisibility(View.GONE);
+            mRlFlowManagement.setVisibility(View.GONE);
+            mRlBatteryManagement.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -51,6 +82,7 @@ public class HomeMoreActivity extends BaseActivity implements View.OnClickListen
     private void initUI() {
         mCtbMain = (CommonToolbar) findViewById(R.id.ctb_main);
         mCtbMain.setToolbarTitle(R.string.lock_more);
+
         mRvIntruderEntry = (RippleView) findViewById(R.id.rv_home_more_intruder);
         mRvIntruderEntry.setOnClickListener(this);
         mRvFindLostEntry = (RippleView) findViewById(R.id.rv_home_more_findlost);
@@ -59,6 +91,20 @@ public class HomeMoreActivity extends BaseActivity implements View.OnClickListen
         mRvBatteryScreenEntry.setOnClickListener(this);
         mRvSettingEntry = (RippleView) findViewById(R.id.rv_home_more_setting);
         mRvSettingEntry.setOnClickListener(this);
+
+        mRvCallfilterEntry = (RippleView) findViewById(R.id.rv_home_more_callfilter);
+        mRvCallfilterEntry.setOnClickListener(this);
+        mRvWifiEntry = (RippleView) findViewById(R.id.rv_home_more_wifi);
+        mRvWifiEntry.setOnClickListener(this);
+        mRvFlowEntry = (RippleView) findViewById(R.id.rv_home_more_flow);
+        mRvFlowEntry.setOnClickListener(this);
+        mRvBatteryEntry = (RippleView) findViewById(R.id.rv_home_more_battery);
+        mRvBatteryEntry.setOnClickListener(this);
+
+        mRlCallFilter = (RelativeLayout) findViewById(R.id.rl_callfilter_content);
+        mRlWifi = (RelativeLayout) findViewById(R.id.rl_wifi_content);
+        mRlFlowManagement = (RelativeLayout) findViewById(R.id.rl_flow_content);
+        mRlBatteryManagement = (RelativeLayout) findViewById(R.id.rl_battery_content);
     }
 
     @Override
@@ -73,12 +119,40 @@ public class HomeMoreActivity extends BaseActivity implements View.OnClickListen
             case R.id.rv_home_more_screenview:
                 goToBatteryScreen();
                 break;
+            case R.id.rv_home_more_callfilter:
+                goToCallfilter();
+                break;
+            case R.id.rv_home_more_wifi:
+                goToWifi();
+                break;
+            case R.id.rv_home_more_flow:
+                goToFlowManagement();
+                break;
+            case R.id.rv_home_more_battery:
+                goToBatteryManagement();
+                break;
             case R.id.rv_home_more_setting:
                 goToMainSetting();
                 break;
             default:
                 break;
         }
+    }
+
+    private void goToBatteryManagement() {
+
+    }
+
+    private void goToFlowManagement() {
+
+    }
+
+    private void goToWifi() {
+
+    }
+
+    private void goToCallfilter() {
+
     }
 
     private void goToMainSetting() {
@@ -112,5 +186,6 @@ public class HomeMoreActivity extends BaseActivity implements View.OnClickListen
         Intent intent = new Intent(this, IntruderprotectionActivity.class);
         startActivity(intent);
     }
+
 }
 
