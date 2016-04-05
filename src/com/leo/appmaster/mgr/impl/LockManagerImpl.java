a3830c0fbe4969c73cbb80b627033366d49cf498
lishuai <lishuai@leomaster.com>
2016-04-05 15:25:10 +0800

    修复应用忽略没有用的问题

--- src/com/leo/appmaster/mgr/impl/LockManagerImpl.java
+++ src/com/leo/appmaster/mgr/impl/LockManagerImpl.java
@@ -1448,6 +1448,7 @@ public class LockManagerImpl extends LockManager {
             public void run() {
                 List<AppItemInfo> itemInfos = AppLoadEngine.getInstance(mContext).getAllPkgInfo();
                 InstalledAppTable.getInstance().insertIgnoreItemList(itemInfos);
+                notifySecurityChange();
             }
         });
         addedScore = addedScore < 0 ? 0 : addedScore;
