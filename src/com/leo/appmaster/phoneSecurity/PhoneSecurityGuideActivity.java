5c26e33737254d8f48decdb019a3a777e5381fb1
RunLee <lilongfei@leoers.com>
2016-04-12 21:22:30 +0800

    去除sim卡检测测试模式

--- src/com/leo/appmaster/phoneSecurity/PhoneSecurityGuideActivity.java
+++ src/com/leo/appmaster/phoneSecurity/PhoneSecurityGuideActivity.java
@@ -21,7 +21,7 @@ import com.leo.appmaster.utils.BuildProperties;
 import com.leo.appmaster.utils.LeoLog;
 
 public class PhoneSecurityGuideActivity extends BaseActivity implements View.OnClickListener {
-    private static final boolean DBG = true;
+    private static final boolean DBG =false;
     private Button mOpenBt;
     private CommonToolbar mCommonBar;
     private TextView mSecurityOpenNumberText;
