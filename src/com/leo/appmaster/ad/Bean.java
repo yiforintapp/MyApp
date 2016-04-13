76736b8d80d93b80f242b9a808f7e1067e9a8837
lilibin <tony@leoers.com>
2016-04-13 15:30:31 +0800

    test

--- src/com/leo/appmaster/ad/Bean.java
+++ src/com/leo/appmaster/ad/Bean.java
@@ -2,8 +2,6 @@ package com.leo.appmaster.ad;
 
 import com.leo.leoadlib.ioc.InjectConfig;
 
-import java.util.Map;
-
 /**
  * 配置sdk调用的业务类的相关信息
  * Created by lilibin on 16-3-23.
@@ -24,7 +22,7 @@ public class Bean {
 	/**
 	 * 配置方法参数列表
 	 */
-	@InjectConfig(paramsList = {Map.class})
+	@InjectConfig(paramsList = "java.util.Map")
 	public Object params;
 
 
