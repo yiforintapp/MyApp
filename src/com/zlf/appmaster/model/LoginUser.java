36de03815c9113912edbdddb7cde92870837ae93
yifulin <313740752@qq.com>
2016-08-26 18:21:59 +0800

    change url

--- src/com/zlf/appmaster/model/LoginUser.java
+++ src/com/zlf/appmaster/model/LoginUser.java
@@ -1,7 +1,5 @@
 package com.zlf.appmaster.model;
 
-import com.zlf.appmaster.model.BaseInfo;
-
 import java.io.Serializable;
 
 /**
@@ -12,7 +10,6 @@ public class LoginUser extends BaseInfo implements Serializable {
     private String phoneNumber;
     private String password;
 
-    //用于URL传参和取参时的key
     public static String PHONENUMBER = "phoneNumber";
     public static String PASSWORD = "passWord";
 
