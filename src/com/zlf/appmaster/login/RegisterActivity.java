fe96b91402d1cb467be1c39be9e4f8ce4357e589
yifulin <313740752@qq.com>
2016-08-29 16:21:57 +0800

    fix string

--- src/com/zlf/appmaster/login/RegisterActivity.java
+++ src/com/zlf/appmaster/login/RegisterActivity.java
@@ -262,7 +262,7 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
         ThreadManager.executeOnAsyncThread(new Runnable() {
             @Override
             public void run() {
-                //新建一个StringBuffer链接
+                // 新建一个StringBuffer链接
                 StringBuffer buffer = new StringBuffer();
 
                 // String encode = "GBK"; //页面编码和短信内容编码为GBK。重要说明：如提交短信后收到乱码，请将GBK改为UTF-8测试。如本程序页面为编码格式为：ASCII/GB2312/GBK则该处为GBK。如本页面编码为UTF-8或需要支持繁体，阿拉伯文等Unicode，请将此处写为：UTF-8
