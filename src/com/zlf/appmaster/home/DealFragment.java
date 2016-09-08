277b04fa843cececc9e090f1039de9cc0026e57f
nicky <535666786@qq.com>
2016-09-08 19:52:45 +0800

    fix some DealFragment

--- src/com/zlf/appmaster/home/DealFragment.java
+++ src/com/zlf/appmaster/home/DealFragment.java
@@ -4,18 +4,27 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.zlf.appmaster.Constants;
 import com.zlf.appmaster.R;
+import com.zlf.appmaster.client.QStringRequest;
 import com.zlf.appmaster.fragment.BaseFragment;
+import com.zlf.appmaster.utils.LeoLog;
+import com.zlf.appmaster.utils.QLog;
 import com.zlf.appmaster.utils.Utils;
+import com.zlf.appmaster.utils.VolleyTool;
 
 /**
  * Created by Administrator on 2016/7/19.
  */
 public class DealFragment extends BaseFragment implements View.OnClickListener {
 
-
+    public final static String TAG = "DealFragment";
+    public final static String CHECK_CJLH = "check_cjlh";
     private TextView mTvJump;
+
 
     @Override
     protected int layoutResourceId() {
@@ -26,6 +35,35 @@ public class DealFragment extends BaseFragment implements View.OnClickListener {
     protected void onInitUI() {
         mTvJump = (TextView) findViewById(R.id.tv_changjiang);
         setListener();
+
+        getPckNameAndUrl();
+    }
+
+    private void getPckNameAndUrl() {
+        String url = Constants.ADDRESS + Constants.SEVLET + Constants.DATA + CHECK_CJLH;
+        LeoLog.d(TAG, "check update url is : " + url);
+
+        QStringRequest stringRequest = new QStringRequest(
+                Request.Method.GET, url, null, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String s) {
+                LeoLog.d(TAG, "check update requestFinished version is : " + s);
+            }
+
+
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                LeoLog.d(TAG, "check update err");
+            }
+
+        });
+
+        // callAll的时候使用
+        VolleyTool.getInstance(mActivity).getRequestQueue()
+                .add(stringRequest);
     }
 
     private void setListener() {
@@ -42,6 +80,8 @@ public class DealFragment extends BaseFragment implements View.OnClickListener {
     }
 
     private void jumpToCJLH() {
-        Utils.startAPP(Constants.CJLH_PACKAGENAME,mActivity);
+        Utils.startAPP(Constants.CJLH_PACKAGENAME, mActivity);
     }
+
+
 }
