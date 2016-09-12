892fd6e08d55af8d99a43b059bff015d94301a6d
yifulin <313740752@qq.com>
2016-09-12 11:03:29 +0800

    modify

---
+++ src/com/zlf/appmaster/login/InfoModifyActivity.java
@@ -0,0 +1,18 @@
+package com.zlf.appmaster.login;
+
+import android.os.Bundle;
+
+import com.zlf.appmaster.R;
+import com.zlf.appmaster.home.BaseActivity;
+
+/**
+ * Created by Administrator on 2016/9/12.
+ */
+public class InfoModifyActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_info);
+    }
+}
