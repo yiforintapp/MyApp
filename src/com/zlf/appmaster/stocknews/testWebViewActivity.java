3550a54a4a6b1e3ac0a7938238bef195f10a1140
yifulin <313740752@qq.com>
2016-09-15 14:28:50 +0800

    fix string 1549

--- src/com/zlf/appmaster/stocknews/testWebViewActivity.java
+++ src/com/zlf/appmaster/stocknews/testWebViewActivity.java
@@ -1,10 +1,8 @@
 package com.zlf.appmaster.stocknews;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.zlf.appmaster.Constants;
@@ -13,6 +11,8 @@ import com.zlf.appmaster.ThreadManager;
 import com.zlf.appmaster.home.BaseActivity;
 
 import io.vov.vitamio.LibsChecker;
+import io.vov.vitamio.MediaPlayer;
+import io.vov.vitamio.widget.MediaController;
 import io.vov.vitamio.widget.VideoView;
 
 
@@ -43,63 +43,63 @@ public class testWebViewActivity extends BaseActivity {
     private void initViews() {
 
         mVideoView = (VideoView) findViewById(R.id.vitamio_videoView);
-//        mVideoView.setMediaController(new MediaController(this));
+        mVideoView.setMediaController(new MediaController(this));
         path = Constants.ZHI_BO_ADDRESS;
-//        mVideoView.setVideoURI(Uri.parse(path));
-//        mVideoView.requestFocus();
-//        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-//            @Override
-//            public void onPrepared(MediaPlayer mediaPlayer) {
-//                mediaPlayer.setPlaybackSpeed(1.0f);
-//            }
-//        });
-//        mVideoView.start();
-//        ThreadManager.getUiThreadHandler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                if (!mVideoView.isPlaying()) {
-//                    mVideoView.start();
-//                }
-//            }
-//        }, 1500);
+        mVideoView.setVideoURI(Uri.parse(path));
+        mVideoView.requestFocus();
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setPlaybackSpeed(1.0f);
+            }
+        });
+        mVideoView.start();
+        ThreadManager.getUiThreadHandler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!mVideoView.isPlaying()) {
+                    mVideoView.start();
+                }
+            }
+        }, 1500);
 
-        mWebView = (WebView) findViewById(R.id.webView);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setDefaultTextEncodingName("UTF-8");
-        webSettings.setBuiltInZoomControls(false);
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-
-        // 回调本地代码
-        webSettings.setJavaScriptEnabled(true);
-
-        webSettings.setAllowContentAccess(true);
-        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
-        webSettings.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
-        webSettings.setAllowFileAccess(true);
-        webSettings.setLoadsImagesAutomatically(true);
-
-        webSettings.setLoadWithOverviewMode(true);
-
-
-
-        mWebView.requestFocus();
-        mWebView.requestFocusFromTouch();
-        mWebView.setFocusable(true);
-        mWebView.setFocusableInTouchMode(true);
-
-        mWebView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
-        mWebView.setVerticalScrollBarEnabled(false);
-        mWebView.setHorizontalScrollBarEnabled(false);
-        mWebView.setDrawingCacheEnabled(true);
-        // 屏蔽长按事件
-        mWebView.setOnCreateContextMenuListener(null);
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setPluginsEnabled(true);
-        webSettings.setPluginState(WebSettings.PluginState.ON);
-        mWebView.setWebChromeClient(new WebChromeClient());
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setUseWideViewPort(true);
-        mWebView.loadUrl("http://vip.zlf1688.com");
+//        mWebView = (WebView) findViewById(R.id.webView);
+//        WebSettings webSettings = mWebView.getSettings();
+//        webSettings.setDefaultTextEncodingName("UTF-8");
+//        webSettings.setBuiltInZoomControls(false);
+//        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+//
+//        // 回调本地代码
+//        webSettings.setJavaScriptEnabled(true);
+//
+//        webSettings.setAllowContentAccess(true);
+//        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+//        webSettings.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+//        webSettings.setAllowFileAccess(true);
+//        webSettings.setLoadsImagesAutomatically(true);
+//
+//        webSettings.setLoadWithOverviewMode(true);
+//
+//
+//
+//        mWebView.requestFocus();
+//        mWebView.requestFocusFromTouch();
+//        mWebView.setFocusable(true);
+//        mWebView.setFocusableInTouchMode(true);
+//
+//        mWebView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+//        mWebView.setVerticalScrollBarEnabled(false);
+//        mWebView.setHorizontalScrollBarEnabled(false);
+//        mWebView.setDrawingCacheEnabled(true);
+//        // 屏蔽长按事件
+//        mWebView.setOnCreateContextMenuListener(null);
+//        webSettings.setJavaScriptEnabled(true);
+//        webSettings.setPluginsEnabled(true);
+//        webSettings.setPluginState(WebSettings.PluginState.ON);
+//        mWebView.setWebChromeClient(new WebChromeClient());
+//        webSettings.setLoadWithOverviewMode(true);
+//        webSettings.setUseWideViewPort(true);
+//        mWebView.loadUrl("http://vip.zlf1688.com");
 
     }
 
